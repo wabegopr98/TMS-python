2eadf94651792731b480d1b0a8ee11e4c864153a
wabegopr98 <rabchuk.serezha2017@yandex.ru>
2024-11-16 18:04:06 +0300

    Done 1 file task

---
+++ HW6/homework6_1.py
@@ -0,0 +1,18 @@
+
+with open("text.txt", "w") as file1:
+    numbers = list(range(1,5))
+    for number in numbers:
+        file1.write(str(number) + " ")
+with open("text.txt", "r") as file1:
+     for line in file1:
+         s = line.split()
+         s = "".join(s)
+         if len(s)<3:
+             print("Слишком мало символов. Нужно больше!")
+
+         else:
+          print(s[0])
+          print(s[1])
+          count = len(s)
+          print(s[count-1])
+          print(s[count-2])
