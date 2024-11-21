b7f8c233cd46e8d667e7d80892819a79d61e3211
wabegopr98 <47750849+wabegopr98@users.noreply.github.com>
2024-11-21 18:53:59 +0300

    Delete Homework3_4 directory

--- Homework3_4/types_1.py
+++
@@ -1,9 +0,0 @@
-"""Перевести строку в массив
-"Robin Singh" => ["Robin”, “Singh"]
-"I love arrays they are my favorite" => ["I", "love", "arrays", "they", "are", "my", "favorite"]"""
-
-text1 = "Robin Singh"
-print(text1.split())
-
-text2 = "I love arrays they are my favorite"
-print(text2.split())
--- Homework3_4/types_3.py
+++
@@ -1,5 +0,0 @@
-"""Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"] сделайте из него
-строку => "I love arrays they are my favorite"""
-
-array1 = ["I", "love", "arrays", "they", "are", "my", "favorite"]
-print(" ".join(array1))
