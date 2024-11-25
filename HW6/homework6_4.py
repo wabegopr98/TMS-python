ba6de44bcd5c035f92adf6e1bc2aaeb7630b7b6b
wabegopr98 <rabchuk.serezha2017@yandex.ru>
2024-11-26 02:28:04 +0300

    Done 6 homework. 2 and 4 task

--- HW6/homework6_4.py
+++ HW6/homework6_4.py
@@ -1,22 +1,23 @@
 """Даны 2 файла произвольного типа. Поменять местами их содержимое.
 Файлы должны быть бинарными."""
+import pickle
 
 with open("new_file1.bin", "wb") as file1:
     my_num = "100000101 00010101"
-    file1.write(my_num.encode())
+    pickle.dump(my_num, file1)
 
 with open("new_file2.bin", "wb") as file2:
     my_num2 = "00101010 11010101"
-    file2.write(my_num2.encode())
+    pickle.dump(my_num2, file2)
 
 with open("new_file1.bin", "rb") as file1:
-    number_lines = file1.read().decode()
+    number_lines = pickle.load(file1)
 
 with open("new_file2.bin", "rb") as file2:
-    number_lines2 = file2.read().decode()
+    number_lines2 = pickle.load(file2)
 
 with open("new_file1.bin", "wb") as file1:
-    file1.write(number_lines2.encode())
+    pickle.dump(number_lines2,file1)
 
 with open("new_file2.bin", "wb") as file2:
-    file2.write(number_lines.encode())
+    pickle.dump(number_lines, file2)
