6ccab6ea0aa48c40cb185fc9785515b1a2e64a81
wabegopr98 <rabchuk.serezha2017@yandex.ru>
2025-02-10 20:02:02 +0300

    Added parametrization. Rewrite final assert

--- hw23/tests/e2e/test_buy_item.py
+++ hw23/tests/e2e/test_buy_item.py
@@ -1,11 +1,19 @@
 import time
+import pytest
 
 from hw23.pages.add_to_cart_page import CartPage
 from hw23.pages.info_buyer_page import BuyerInfo
 from hw23.pages.login_page import LoginPage
 
 
-def test_buy_item(driver):
+@pytest.mark.parametrize(
+    ("first_name", "second_name", "zip"), [
+        ("Sergey", "Ivanov", "123456"),
+        ("Andrey", "Vanyushov", "987654"),
+        ("Anastasiya", "Korovina", "192837"),
+    ]
+)
+def test_buy_item(driver,first_name, second_name, zip):
     good_login = LoginPage(driver)
     good_login.open_url("https://www.saucedemo.com/")
     good_login.enter_username("standard_user")
@@ -21,13 +29,14 @@ def test_buy_item(driver):
     my_item.click_checkout()
     time.sleep(3)
 
+
     continue_with_item = BuyerInfo(driver)
-    continue_with_item.enter_first_name()
-    continue_with_item.enter_second_name()
-    continue_with_item.enter_zip()
+    continue_with_item.enter_first_name(first_name)
+    continue_with_item.enter_second_name(second_name)
+    continue_with_item.enter_zip(zip)
     time.sleep(3)
     continue_with_item.click_continue()
     time.sleep(3)
     continue_with_item.click_finish()
     time.sleep(3)
-    assert continue_with_item.successfull_buy() == "Thank you for your order!"
+    assert "checkout-complete" in driver.current_url
